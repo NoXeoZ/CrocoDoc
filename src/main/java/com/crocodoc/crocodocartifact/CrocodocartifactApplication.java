73ad5b4abc49df859abef3412c01b63d30e4575e
unknown <d.randrianjatovo@gmail.com>
2019-12-20 11:21:14 +0100

    add project

---
+++ src/main/java/com/crocodoc/crocodocartifact/CrocodocartifactApplication.java
@@ -0,0 +1,13 @@
+package com.crocodoc.crocodocartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrocodocartifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrocodocartifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crocodoc/crocodocartifact/CrocodocartifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crocodoc.crocodocartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrocodocartifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
