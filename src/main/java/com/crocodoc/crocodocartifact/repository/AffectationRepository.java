a2d1dc643179a9515f3934e515728075bae00bcd
kenzayahi <yahikenza865@gmail.com>
2020-01-18 16:53:17 +0100

    Gestion Affectations

---
+++ src/main/java/com/crocodoc/crocodocartifact/repository/AffectationRepository.java
@@ -0,0 +1,9 @@
+package com.crocodoc.crocodocartifact.repository;
+
+import com.crocodoc.crocodocartifact.model.Affectation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AffectationRepository  extends CrudRepository<Affectation,Long> {
+}
