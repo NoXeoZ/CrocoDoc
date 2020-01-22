3d3d79fb9f2699520e393c1d3ff1e32255c80e2b
CHarranger <cyrilharranger@orange.fr>
2020-01-22 15:17:00 +0100

    Ajout des Repository

--- src/main/java/com/crocodoc/crocodocartifact/repository/DMPRepository.java
+++ src/main/java/com/crocodoc/crocodocartifact/repository/DMPRepository.java
@@ -2,6 +2,8 @@ package com.crocodoc.crocodocartifact.repository;
 
 import com.crocodoc.crocodocartifact.model.DMP;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DMPRepository extends JpaRepository<DMP, Long> {
 }
