05f6002611b67d636589d6395b996565ac8d13f4
Julien <jpiegle@yahoo.fr>
2020-01-25 19:05:01 +0100

    add ids

--- src/main/java/com/crocodoc/crocodocartifact/model/Affectation.java
+++ src/main/java/com/crocodoc/crocodocartifact/model/Affectation.java
@@ -1,8 +1,14 @@
 package com.crocodoc.crocodocartifact.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.AUTO;
 
 @Entity
 public class Affectation {
-
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private long id;
 }
