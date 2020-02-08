822700ae455bb8235bfa6bce9526d422735fcf52
CHarranger <cyrilharranger@orange.fr>
2020-02-08 14:25:57 +0100

    ebauche

---
+++ src/main/java/com/crocodoc/crocodocartifact/model/ActType.java
@@ -0,0 +1,5 @@
+package com.crocodoc.crocodocartifact.model;
+
+public enum ActType {
+
+}
--- src/main/java/com/crocodoc/crocodocartifact/model/DMP.java
+++ src/main/java/com/crocodoc/crocodocartifact/model/DMP.java
@@ -1,8 +1,45 @@
 package com.crocodoc.crocodocartifact.model;
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
+@Table(name = "dmp")
 public class DMP {
+    @Id
+    @GeneratedValue
+    private long id;
 
+    @OneToMany(mappedBy = "dmp", fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Hospitalisation> hospitalisations = new ArrayList<>();
+
+    @OneToMany(mappedBy = "dmp", fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Act> actes = new ArrayList<>();
+
+
+    /** Juste for JPA */
+    protected DMP() {}
+
+
+    public List<Hospitalisation> getHospitalisations() {
+        return new ArrayList<>(hospitalisations);
+    }
+
+    public void addHospitalisation(Hospitalisation o) {
+        hospitalisations.add(Objects.requireNonNull(o));
+    }
+
+    public List<Act> getActes() {
+        return new ArrayList<>(actes);
+    }
+
+    public void addActe(Act o) {
+        actes.add(Objects.requireNonNull(o));
+    }
 }
