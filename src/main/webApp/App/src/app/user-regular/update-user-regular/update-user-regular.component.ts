fe439bf1f36bd70bad5dd2ee6451055f81eab272
NoXeoZ <d.randrianjatovo@gmail.com>
2020-03-18 16:54:54 +0100

    user regular update ok

--- src/main/webApp/App/src/app/user-regular/update-user-regular/update-user-regular.component.ts
+++ src/main/webApp/App/src/app/user-regular/update-user-regular/update-user-regular.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {TypeUser, User} from "../../model/user";
+import {Structure} from "../../model/structure";
+import {ActivatedRoute, Router} from "@angular/router";
+import {UserService} from "../../user/user.service";
+import {StructureService} from "../../structure/structure.service";
 
 @Component({
   selector: 'app-update-user-regular',
@@ -7,9 +13,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateUserRegularComponent implements OnInit {
 
-  constructor() { }
+  id:number;
+  formGroup: FormGroup;
+  types =[ TypeUser.SECRETARY,TypeUser.NURSE,TypeUser.DOCTOR,TypeUser.LAB_STAFF,TypeUser.ADMIN];
 
+  startDate = new Date(1970, 0, 1);
+
+
+
+  @Output()
+  updateUser=new EventEmitter<User>();
+  private key: string;
+  hide = true;
+
+  email1 = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  private structures:Structure[];
+
+  constructor(protected router: Router,private userService : UserService, private route: ActivatedRoute,private structureService : StructureService) { }
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.key = this.route.snapshot.params['key'];
+    this.structureService.getStructures(this.key)
+      .subscribe(s => {
+        this.structures = s as Structure[]
+      })
+
+    this.userService.getUser(this.id, this.key).subscribe(data => {
+      let email1 = new FormControl(data.email, [
+        Validators.required,
+        Validators.email,
+      ]);
+      this.formGroup = new FormGroup({
+        id: new FormControl(data.id),
+        firstname: new FormControl(data.firstname),
+        lastname: new FormControl(data.lastname),
+        birthDate: new FormControl(data.birthDate),
+        address: new FormControl('', [Validators.required]),
+        phonenumber: new FormControl('', [Validators.required]),
+        email: new FormControl(data.email,[Validators.required]),
+        rib: new FormControl(data.RIB),
+        password: new FormControl(data.password),
+        structure: new FormControl(data.structure),
+        type: new FormControl(data.type),
+      });
+
+
+    })
+
+
+  }
+  onUpdateUser() {
+    let user: User =  this.formGroup.value;
+    console.log("HEEEEEEEEEEEEEEEERRRRRRRRRRREEEEEEEEEEEEE")
+    this.userService.createUser(user,this.key).subscribe(
+      data => {this.updateUser.emit(user);
+        this.router.navigate(['/user/' + this.key]).then(r  =>console.log("update Ok"))},
+      error => console.log(error)
+    );
+    //}
   }
 
 }
